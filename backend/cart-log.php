813cd2b7138304f698cc981748d3f9ee456e54c8
RomanPaulaIoana <100189199+RomanPaulaIoana@users.noreply.github.com>
2023-06-18 20:44:49 +0300

    Add files via upload

---
+++ backend/cart-log.php
@@ -0,0 +1,138 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+
+if(isset($_GET['delete'])){
+    $delete_id = $_GET['delete'];
+    mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$delete_id'") or die('query failed');
+    header('location:cart.php');
+}
+
+if(isset($_GET['delete_all'])){
+    mysqli_query($conn, "DELETE FROM `cart` WHERE id_user = '$user_id'") or die('query failed');
+    header('location:cart-log.php');
+};
+
+if(isset($_POST['update_quantity'])){
+    $cart_id = $_POST['cart_id'];
+    $cart_quantity = $_POST['cart_quantity'];
+    mysqli_query($conn, "UPDATE `cart` SET Quantity = '$cart_quantity' WHERE id_flower = '$cart_id'") or die('query failed');
+    $message[] = 'cart quantity updated!';
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Shop</title>
+  <link rel="stylesheet" href="styles/style-fav.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="js/script.js"></script>
+</head>
+
+<body>
+  <header class="header">
+    <div class="logo-wrapper">
+      <a class="logo-a" href="home-not-logged.html"><img class="logo-img" src="styles/resources/logo-image.png" alt="Plant Palooza"></a>
+    </div>
+    <input class="search-input" type="text" placeholder="search" />
+
+    <div class="side-header">
+      <div class="account-div">
+        <a class="account-a" href="login.html">
+          <img class="account-img" src="https://static.thenounproject.com/png/4038155-200.png" alt="account">
+        </a>
+      </div>
+      
+      <div class="favorite-a">
+        <a href="favorite.html">
+          <img class="fav-img" src="styles/resources/favorite.png" alt="favorite">
+        </a>
+      </div>
+      <div class="dropdown2">
+        <img class="shopping-cart-img" src="https://icons.veryicon.com/png/o/miscellaneous/jd-app-icon/shopping-cart-296.png" alt="shopping cart">
+      </div>
+    </div>
+  </header>
+  <?php if (!empty($message)): ?>
+  <div class="message-container">
+    <div class="message-box">
+    <div class="message"><?php echo implode(', ', $message); ?></div>
+    </div>
+  </div>
+<?php endif; ?>
+  
+  <div class="menu">
+    <a href="home-logged-user.html">H O M E</a>
+    <a class="active" href="shop-log.html">S H O P</a>
+    <a href="about-log.html">A B O U T</a>
+    <a href="help-log.html">H E L P</a>
+    <a href="help-log.html">C R O P S</a>
+  </div>
+
+  <section class="shopping-cart">
+
+    <h1 class="title">products added</h1>
+
+    <div class="box-container">
+
+    <?php
+        $grand_total = 0;
+        $select_cart = mysqli_query($conn, "SELECT * FROM `cart` WHERE id_user = '$user_id'") or die('query failed');
+        if(mysqli_num_rows($select_cart) > 0){
+            while($fetch_cart = mysqli_fetch_assoc($select_cart)){
+    ?>
+    <div  class="box">
+                <div class="image">
+                    <img src="<?php echo $fetch_cart['imageURL']; ?>" alt="Flower Image">
+                </div>
+        <div class="name"><?php echo $fetch_cart['Name']; ?></div>
+        <div class="price">$<?php echo $fetch_cart['Price']; ?>/-</div>
+        <form action="" method="post">
+            <input type="hidden" value="<?php echo $fetch_cart['id_flower']; ?>" name="cart_id">
+            <input type="number" min="1" value="<?php echo $fetch_cart['Quantity']; ?>" name="cart_quantity" class="qty">
+            <input type="submit" value="update" class="option-btn" name="update_quantity">
+        </form>
+        <div class="sub-total"> sub-total : <span>$<?php echo $sub_total = ($fetch_cart['Price'] * $fetch_cart['Quantity']); ?>/-</span> </div>
+    </div>
+    <?php
+    $grand_total += $sub_total;
+        }
+    }else{
+        echo '<p class="empty">your cart is empty</p>';
+    }
+    ?>
+    </div>
+
+    <div class="more-btn">
+        <a href="cart-log.php?delete_all" class="delete-btn <?php echo ($grand_total > 1)?'':'disabled' ?>" onclick="return confirm('delete all from cart?');">delete all</a>
+    </div>
+
+    <div class="cart-total">
+        <p>grand total : <span>$<?php echo $grand_total; ?>/-</span></p>
+        <a href="shop-log.php" class="option-btn">continue shopping</a>
+        <a href="checkout.php" class="btn  <?php echo ($grand_total > 1)?'':'disabled' ?>">proceed to checkout</a>
+    </div>
+
+</section>
+
+
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ backend/shop-log.php
@@ -0,0 +1,140 @@
+<?php
+  @include 'config.php';
+  session_start();
+  $user_id = $_SESSION['user_id'];
+  $message = ''; // Variabila pentru a stoca mesajul
+  
+  if (!isset($user_id)) {
+    header('location: login.html');
+    exit;
+  }
+
+  if (isset($_POST['add_to_wishlist'])) {
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+
+    $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `favorites` WHERE ID_user = '$user_id' AND ID_flower = '$product_id'") or die('query failed');
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE id_user = '$user_id' AND id_flower = '$product_id'") or die('query failed');
+
+    if (mysqli_num_rows($check_wishlist_numbers) > 0) {
+      $message = 'already added to wishlist';
+    } elseif (mysqli_num_rows($check_cart_numbers) > 0) {
+      $message = 'already added to cart';
+    } else {
+      mysqli_query($conn, "INSERT INTO `favorites` (ID_user, ID_flower, Name, Price, ImageURL) VALUES ('$user_id', '$product_id', '$product_name', '$product_price', '$product_image')") or die('query failed');
+      $message = 'product added to wishlist';
+    }
+  }
+
+  if (isset($_POST['add_to_cart'])) {
+    $product_id = $_POST['product_id'];
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $product_quantity = $_POST['product_quantity'];
+
+    $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE ID_user = '$user_id' AND ID_flower = '$product_id'") or die('query failed');
+
+    if (mysqli_num_rows($check_cart_numbers) > 0) {
+      $message = 'already added to cart';
+    } else {
+      $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE id_user = '$user_id' AND id_flower = '$product_id'") or die('query failed');
+
+      if (mysqli_num_rows($check_wishlist_numbers) > 0) {
+        mysqli_query($conn, "DELETE FROM `wishlist` WHERE ID_user = '$user_id' AND ID_flower = '$product_id'") or die('query failed');
+      }
+
+      mysqli_query($conn, "INSERT INTO `cart` (id_user, id_flower, Name, Price, Quantity, ImageURL) VALUES ('$user_id', '$product_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
+      $message = 'product added to cart';
+    }
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Shop</title>
+  <link rel="stylesheet" href="styles/style-shop.css">
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="js/script.js"></script>
+</head>
+
+<body>
+  <header class="header">
+    <div class="logo-wrapper">
+      <a class="logo-a" href="home-not-logged.html"><img class="logo-img" src="styles/resources/logo-image.png" alt="Plant Palooza"></a>
+    </div>
+    <input class="search-input" type="text" placeholder="search" />
+
+    <div class="side-header">
+      <div class="account-div">
+        <a class="account-a" href="login.html">
+          <img class="account-img" src="https://static.thenounproject.com/png/4038155-200.png" alt="account">
+        </a>
+      </div>
+      
+      <div class="favorite-a">
+        <a href="favorite-log.php">
+          <img class="fav-img" src="styles/resources/favorite.png" alt="favorite">
+        </a>
+      </div>
+      <div class="dropdown2">
+      <a href="cart-log.php">
+        <img class="shopping-cart-img" src="https://icons.veryicon.com/png/o/miscellaneous/jd-app-icon/shopping-cart-296.png" alt="shopping cart">
+      </div>
+    </div>
+  </header>
+  <?php if (!empty($message)): ?>
+  <div class="message-container">
+    <div class="message-box">
+      <div class="message"><?php echo $message; ?></div>
+    </div>
+  </div>
+<?php endif; ?>
+  
+  <div class="menu">
+    <a href="home-logged-user.html">H O M E</a>
+    <a class="active" href="shop-log.html">S H O P</a>
+    <a href="about-log.html">A B O U T</a>
+    <a href="help-log.html">H E L P</a>
+    <a href="help-log.html">C R O P S</a>
+  </div>
+
+  <section class="products">
+    <div class="box-container">
+      <?php
+        $select_products = mysqli_query($conn, "SELECT * FROM `flowers`") or die('query failed');
+        if (mysqli_num_rows($select_products) > 0) {
+          while ($fetch_products = mysqli_fetch_assoc($select_products)) {
+      ?>
+            <form action="" method="POST" class="box">
+              <div class="name">
+                <a href="plant_page.html?pid=<?php echo $fetch_products['ID_flower']; ?>">
+                  <span style="font-size: 40px;"><?php echo $fetch_products['Name']; ?></span>
+                </a>
+              </div>
+              <div class="price">$<?php echo $fetch_products['Price']; ?>/-</div>
+              <img src="<?php echo $fetch_products['ImageURL']; ?>" alt="" class="image">
+              <input type="number" name="product_quantity" value="1" min="0" class="qty">
+              <input type="hidden" name="product_id" value="<?php echo $fetch_products['ID_flower']; ?>">
+              <input type="hidden" name="product_name" value="<?php echo $fetch_products['Name']; ?>">
+              <input type="hidden" name="product_price" value="<?php echo $fetch_products['Price']; ?>">
+              <input type="hidden" name="product_image" value="<?php echo $fetch_products['ImageURL']; ?>">
+              <input type="submit" value="add to wishlist" name="add_to_wishlist" class="option-btn">
+              <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+            </form>
+      <?php
+          }
+        } else {
+          echo '<p class="empty">no products added yet!</p>';
+        }
+      ?>
+    </div>
+  </section>
+</body>
+</html>
